68700a1e4f96771f55d7bedccee7701159cdfde8
vupham-rainn <hoangvuka2004@gmail.com>
2025-05-27 00:53:38 +0700

    [add] lab-02 railfence

--- LAB-02/api.py
+++ LAB-02/api.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from cipher.caesar import CaesarCipher
 from cipher.vigenere import VigenereCipher
-# from cipher.railfence import RailFenceCipher
+from cipher.railfence import RailFenceCipher
 # from cipher.playfair import PlayFairCipher
 # from cipher.transposition import TranspositionCipher
 
@@ -45,24 +45,24 @@ def vigenere_decrypt():
     decrypted_text = vigenere_cipher.vigenere_decrypt(cipher_text, key)
     return jsonify({'decrypted_text': decrypted_text})
 
-# #RAILFENCE CIPHER ALGORITHM
-# railfence_cipher = RailFenceCipher()
+# RAILFENCE CIPHER ALGORITHM
+railfence_cipher = RailFenceCipher()
 
-# @app.route('/api/railfence/encrypt', methods=['POST'])
-# def encrypt():
-#     data = request.json
-#     plain_text = data['plain_text']
-#     key = int(data['key'])
-#     encrypted_text = railfence_cipher.rail_fence_encrypt(plain_text, key)
-#     return jsonify({'encrypted_text': encrypted_text})
+@app.route('/api/railfence/encrypt', methods=['POST'])
+def encrypt():
+    data = request.json
+    plain_text = data['plain_text']
+    key = int(data['key'])
+    encrypted_text = railfence_cipher.rail_fence_encrypt(plain_text, key)
+    return jsonify({'encrypted_text': encrypted_text})
 
-# @app.route('/api/railfence/decrypt', methods=['POST'])
-# def decrypt():
-#     data = request.json
-#     cipher_text = data['cipher_text']
-#     key = int(data['key'])
-#     decrypted_text = railfence_cipher.rail_fence_decrypt(cipher_text, key)
-#     return jsonify({'decrypted_text': decrypted_text})
+@app.route('/api/railfence/decrypt', methods=['POST'])
+def decrypt():
+    data = request.json
+    cipher_text = data['cipher_text']
+    key = int(data['key'])
+    decrypted_text = railfence_cipher.rail_fence_decrypt(cipher_text, key)
+    return jsonify({'decrypted_text': decrypted_text})
 
 # #PLAYFAIR CIPHER AlGORITHM
 # playfair_cipher = PlayFairCipher()
