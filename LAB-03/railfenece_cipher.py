6385b14c53bbd57ec0399fbbeb1c7fd8264d866f
vupham-rainn <hoangvuka2004@gmail.com>
2025-06-03 15:54:48 +0700

    [add] lab-03 ui vigenere

---
+++ LAB-03/railfenece_cipher.py
@@ -0,0 +1,60 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
+from ui.railfenece import Ui_MainWindow  # Giả sử bạn đã có file ui/railfence.py từ Qt Designer
+import requests
+
+class MyApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.btn_encrypt.clicked.connect(self.call_api_encrypt)
+        self.ui.btn_decrypt.clicked.connect(self.call_api_decrypt)
+
+    def call_api_encrypt(self):
+        url = "http://127.0.0.1:5000/api/railfence/encrypt"
+        payload = {
+            "plain_text": self.ui.txt_plain.toPlainText(),
+            "key": self.ui.txt_key.toPlainText()
+        }
+        try:
+            response = requests.post(url, json=payload)
+            if response.status_code == 200:
+                data = response.json()
+                self.ui.txt_cipher.setPlainText(data["encrypted_message"])  # Đồng bộ với Flask
+                msg = QMessageBox()
+                msg.setIcon(QMessageBox.Information)
+                msg.setText("Encrypted Successfully")
+                msg.exec_()
+            else:
+                print("API Error:", response.status_code)
+                print("Response:", response.text)
+        except Exception as e:
+            print("Error:", str(e))
+
+    def call_api_decrypt(self):
+        url = "http://127.0.0.1:5000/api/railfence/decrypt"
+        payload = {
+            "cipher_text": self.ui.txt_cipher.toPlainText(),
+            "key": self.ui.txt_key.toPlainText()
+        }
+        try:
+            response = requests.post(url, json=payload)
+            if response.status_code == 200:
+                data = response.json()
+                self.ui.txt_plain.setPlainText(data["decrypted_message"])
+                msg = QMessageBox()
+                msg.setIcon(QMessageBox.Information)
+                msg.setText("Decrypted Successfully")
+                msg.exec_()
+            else:
+                print("API Error:", response.status_code)
+                print("Response:", response.text)
+        except Exception as e:
+            print("Error:", str(e))
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MyApp()
+    window.show()
+    sys.exit(app.exec_())
